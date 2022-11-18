f506eae7c342379f5bacc7c79302e7fe84d30276
Thompson Plyler <nealok.g@gmail.com>
2022-11-18 16:26:13 -0500

    Add preliminary fields.

---
+++ form/form.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function form() {
+  return (
+    <form>
+      <input type="text"></input>
+      <input type="password"></input>
+    </form>
+  );
+}
